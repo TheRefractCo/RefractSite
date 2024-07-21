21b0d6a22427f8c7885b3d779bc8023328b2eaf0
shef3r <sheferdev@proton.me>
2024-07-21 16:33:01 +0200

    added gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: '/RefractSite/',
 	plugins: [sveltekit(), purgeCss()]
 });
