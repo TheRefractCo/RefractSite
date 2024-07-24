bb269cae14710b07ea2991a359452aa1901be42c
Aurumaker72 <armkdev72@gmail.com>
2024-07-24 12:00:10 +0200

    Add aurumaker card

--- src/components/cards/AurumakerCard.svelte
+++ src/components/cards/AurumakerCard.svelte
@@ -1,10 +1,20 @@
-<div class="smalltxt">aurumaker didn't make their member card yet.</div>
+<script>
+	import aurumakerLogo from './aurumaker/aurumaker.png';
+	import github from '../../github.svg';
+</script>
 
-<style>
-	.smalltxt {
-		letter-spacing: -1px;
-		margin-bottom: 20px;
-		font-weight: 100;
-		font-size: 18px;
-	}
-</style>
+<div class="flex flex-col gap-8">
+	<div class="flex flex-row items-center gap-8">
+		<img class="object-fill" src={aurumakerLogo} alt="Aurumaker72" width="30%" />
+		<p>Aurumaker72</p>
+	</div>
+
+	<p>Solutions engineer, DMS developer</p>
+	
+	<a href="https://github.com/Aurumaker72" id="gh">
+		<div class="flex flex-row items-center gap-4">
+			<div class="iconcont"><img src={github} alt="GitHub" class="w-[30px]" /></div>
+			<p>@Aurumaker72</p>
+		</div>
+	</a>
+</div>
